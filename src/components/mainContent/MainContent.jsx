49d3ee27f7705ced298004bc79b4afcda24f7bc3
KSkryabin <Kirill.Skryabin@envision.design>
2021-03-24 01:06:58 +0300

    save

--- src/components/mainContent/MainContent.jsx
+++ src/components/mainContent/MainContent.jsx
@@ -1,44 +1,74 @@
-import React from 'react';
-import {AppBar, Avatar, Box, Toolbar, Typography} from '@material-ui/core';
+import React from "react";
+import { AppBar, Avatar, Box, Toolbar, Typography } from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
+import Grid from "@material-ui/core/Grid";
 
-import {useStyles} from "@components/mainContent/style";
+import { useStyles } from "@components/mainContent/style";
 
 const MainContent = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
+      <AppBar position="static" color="primary">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             Bad Forecast
           </Typography>
-          <Avatar alt="Cindy Baker" src="http://healthysoulmag.com/wp-content/uploads/2019/04/hellomagazine.com_.jpg" />
-          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <div>dddddd</div>
-            <div>dddd</div></Box>
+          <Avatar
+            className={classes.avatar}
+            alt="Cindy Baker"
+            src="http://healthysoulmag.com/wp-content/uploads/2019/04/hellomagazine.com_.jpg"
+          />
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+          >
+            <div className={classes.userName}>bad.forecast</div>
+            <div className={classes.role}>Admin</div>
+          </Box>
         </Toolbar>
       </AppBar>
-      <List component="nav" className={classes.root} aria-label="mailbox folders">
-        <ListItem button>
-          <ListItemText primary="Inbox" />
-        </ListItem>
-        <Divider />
-        <ListItem button divider>
-          <ListItemText primary="Drafts" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Trash" />
-        </ListItem>
-        <Divider light />
-        <ListItem button>
-          <ListItemText primary="Spam" />
-        </ListItem>
-      </List>
+
+      <Grid container spacing={3}>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={3}>
+          <List
+            component="nav"
+            className={classes.list}
+            aria-label="mailbox folders"
+          >
+            <ListItem button>
+              <ListItemText primary="Headcount" />
+            </ListItem>
+            <Divider />
+            <ListItem button divider>
+              <ListItemText primary="Plan" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Employees" />
+            </ListItem>
+            <Divider light />
+            <ListItem button>
+              <ListItemText primary="Positions" />
+            </ListItem>
+            <Divider light />
+            <ListItem button>
+              <ListItemText primary="Teams" />
+            </ListItem>
+            <Divider light />
+            <ListItem button>
+              <ListItemText primary="System Users" />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={8}></Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/mainContent/style.js
+++ src/components/mainContent/style.js
@@ -1,16 +1,30 @@
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        borderTop: '1px solid #DCDCDC',
-        borderRadius: '5px',
-    },
-    title: {
-        flexGrow: 1,
-        marginLeft: theme.spacing(16),
-        fontWeight: 'bold',
-    },
+  root: {
+    flexGrow: 1,
+    borderTop: "1px solid #DCDCDC",
+    borderRadius: "5px",
+    minHeight: "700px",
+  },
+  title: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(16),
+    fontWeight: "bold",
+  },
+  avatar: {
+    marginRight: "15px",
+  },
+  userName: {
+    fontWeight: "bold",
+  },
+  role: {
+    fontSize: "12px",
+  },
+  list: {
+    marginTop: "10px",
+    background: "#E6E6FA",
+  },
 }));
 
-export {useStyles}
+export { useStyles };
