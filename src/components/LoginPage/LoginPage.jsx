6f09b51d2da84e7ad66450177d1998d4916b42e3
KSkryabin <Kirill.Skryabin@envision.design>
2021-03-26 00:37:14 +0300

    save

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import { observer } from 'mobx-react';
-import { Box } from '@material-ui/core';
-
-import { authState } from '@store/authState';
-import { Button } from '@ui/Button/Button';
+import React from "react";
+import { observer } from "mobx-react";
+import { Box } from "@material-ui/core";
+import { Button } from "@ui/Button/Button";
 
 const LoginPage = observer(() => {
   return (
-    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       <h1>Bad Forecast</h1>
-      <Button
-        onClick={() => {
-          authState.makeAuthTruthy();
-        }}
-        variant="contained"
-        color="primary"
-        width="200px"
-      >
+      <Button type="submit" variant="contained" color="primary" width="200px">
         LOGIN
       </Button>
     </Box>
